da19bf1bc0fb10a63f56a741249a90711c1dca00
aayancp16-crypto <aayancp16@gmail.com>
2026-02-25 11:22:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,9 @@ def checkout():
     return "Payment system will be integrated here."
 
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
+
+    
+
 
